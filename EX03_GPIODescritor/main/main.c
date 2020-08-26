f924ba7addaf74cde22f098f30438804393b6b0c
Vagner Rodrigues <vagner.silva.rodrigues@gmail.com>
2020-08-26 12:04:10 -0300

    Inclusão do EX03

--- EX03_GPIODescritor/main/main.c
+++ EX03_GPIODescritor/main/main.c
@@ -1,7 +1,7 @@
 /*
 	Autor: Prof. Vagner Rodrigues
 	Objetivo: Manipulação das GPIOs utilizando SDK-IDF com RTOS (FreeRTOS)
-			  Controlando GPIO por Tasks
+			  Configurando GPIOs através de descritores
 	Disciplina: IoT Aplicada
 	Curso: Engenharia da Computação
 */
@@ -21,6 +21,8 @@
 #define LED_G			GPIO_NUM_12
 #define LED_B 			GPIO_NUM_14
 #define BUTTON			GPIO_NUM_16 
+#define GPIO_OUTPUT_PIN_SEL  	((1ULL<<LED_G) | (1ULL<<LED_B))
+#define GPIO_INPUT_PIN_SEL  	(1ULL<<BUTTON)
 
 /* Protótipos de Funções */
 void app_main( void );
@@ -39,9 +41,9 @@ void task_GPIO_Blink( void *pvParameter )
 		GPIO_MODE_INPUT_OUTPUT 			//Dreno Aberto
     */
 	gpio_pad_select_gpio( LED_R ); 	
-	gpio_set_direction( LED_R, GPIO_MODE_OUTPUT );
-    bool estado = 0;   
-
+	gpio_set_direction( LED_R, GPIO_MODE_OUTPUT );	
+	bool estado = 0; 
+	
     while ( TRUE ) 
     {		
 		estado = !estado;
@@ -60,20 +62,27 @@ void task_GPIO_Control( void *pvParameter )
 		GPIO_MODE_INPUT        			//Entrada
 		GPIO_MODE_INPUT_OUTPUT 			//Dreno Aberto
     */
-	gpio_pad_select_gpio( LED_G );
-	gpio_pad_select_gpio( LED_B ); 	
-	gpio_set_direction( LED_G, GPIO_MODE_OUTPUT );	
-	gpio_set_direction( LED_B, GPIO_MODE_OUTPUT );	
 	
+	gpio_config_t io_conf;  //Declaração da variável descritora. 
+    io_conf.intr_type = GPIO_PIN_INTR_DISABLE; //Desabilita interrupção externa.
+    io_conf.mode = GPIO_MODE_OUTPUT; //Configura GPIO como saídas.
+    io_conf.pin_bit_mask = GPIO_OUTPUT_PIN_SEL; //Carrega GPIO configuradas.
+    gpio_config( &io_conf );  //Configura GPIO conforme descritor.
+  
+  
 	/*	Parâmetros de controle da resistência interna na função "gpio_set_pull_mode"
 		GPIO_PULLUP_ONLY,               // Pad pull up            
 		GPIO_PULLDOWN_ONLY,             // Pad pull down          
 		GPIO_PULLUP_PULLDOWN,           // Pad pull up + pull down
 		GPIO_FLOATING,                  // Pad floating  
 	*/
-	gpio_pad_select_gpio( BUTTON ); 
-	gpio_set_direction( BUTTON, GPIO_MODE_INPUT );
-	gpio_set_pull_mode( BUTTON, GPIO_PULLUP_ONLY );       
+	io_conf.intr_type = GPIO_PIN_INTR_DISABLE;  //Desabilita interrupção externa.
+    io_conf.mode = GPIO_MODE_INPUT; //Configura GPIO como entradas.
+    io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL; //Carrega GPIO configuradas.
+    io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE; //Desabilita Pull-down das GPIO's.
+    io_conf.pull_up_en = GPIO_PULLUP_ENABLE; //Habilita Pull-up das GPIO's.
+    gpio_config(&io_conf);    //Configura GPIO conforme descritor.
+    
 
     while ( TRUE ) 
     {
